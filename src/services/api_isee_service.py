088203968e028a83572f0dbbb75a4ac336cf87fc
raha-nug <rivalnugraha5@gmail.com>
2023-01-22 13:30:55 +0700

    add

--- src/services/api_isee_service.py
+++ src/services/api_isee_service.py
@@ -7,15 +7,20 @@ isee = Blueprint("isee", __name__, url_prefix="/api-isee")
 @isee.get("/")
 def getAllData():
    responseLogin = loginAPP()
-   token = responseLogin.token
-   
    return responseLogin
 
 
 def loginAPP():
    username = os.environ.get("USER_CREDENTIAL")
    password = os.environ.get("PASS_CREFENTIAL")
+   db = os.environ.get("DB_ASSET")
 
    url = requests.post("https://isee.icareweb.com/apiv4/login/",json={"username": username,"password": password})
+   loginResponse = url.json()
+   token_auth = loginResponse["token"]
 
-   return url.json()
+   header = {'Authorization': 'Bearer {}'.format(token_auth)}
+   db_access = requests.get(f"https://isee.icareweb.com/apiv4/login/{db}", headers=header)
+   db_token = db_access.json()
+
+   return db_token["token"]
